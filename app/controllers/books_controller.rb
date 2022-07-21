6e9d1b189966589c1d8c545aa6f4a20df490ab9b
EC2 Default User <ec2-user@ip-172-31-5-192.ap-northeast-1.compute.internal>
2022-07-21 15:04:58 +0000

    [Update]ルーティング記述変更

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,11 @@
+class BooksController < ApplicationController
+  def index
+    @books =Book.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "5fbe3353543449e98a1e674b1135d967.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
